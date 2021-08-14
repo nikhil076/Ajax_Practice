6315ec654844fb0b16526d1881aefcd73b8e1e4e
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-14 16:07:43 +0530

    Added: Code added to demonstrate async nature of javascript

--- timer.js
+++ timer.js
@@ -1,1 +1,12 @@
-console.log("Ajax practice");
+function showTime() {
+    const date = new Date();
+    return date.getHours() + "Hrs:" + date.getMinutes() + "Mins:" + date.getSeconds() + "Secs";
+}
+
+function showSessionExpire() {
+    console.log("Activity-B: Session expired at " + showTime());
+}
+
+console.log("Activity-A: Triggering Activity-B at " + showTime());
+setTimeout(showSessionExpire, 5000);
+console.log("Activity-A: Triggered Activity-B at " + showTime() + " wil expire after 5 seconds");
