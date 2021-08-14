ca78794cf9c39496c9a8ef345b1050c4bdfb5746
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-14 23:13:12 +0530

    Added : The ajax promise function is called again instead of onreadystatechange listener set onload and onerror listener used

---
+++ http_service.js
@@ -0,0 +1,31 @@
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            console.log(methodType + "State change called. Ready state: " + xhr.readyState + "Status: " + xhr.status);
+            if (xhr.status === 200 || xhr.status === 201)
+                resolve(xhr.responseText);
+            else if (xhr.status >= 400) {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("XHR failed");
+            }
+        }
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: this.statusText
+            });
+        };
+        xhr.open(methodType, url, async);
+        if (data) {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        }
+        else xhr.send();
+        console.log(methodType + " request sent to the server");
+    });
+}
